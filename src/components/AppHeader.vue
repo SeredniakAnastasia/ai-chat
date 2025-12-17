33d729af108fc940f1c95df52784f806f82205c8
Anastasiia Seredniak <anastasia@MacBook-Air-Anastasiia.local>
2025-12-17 18:39:06 +0200

    Update responsive

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,11 +2,11 @@
   <v-app-bar>
     <v-container fluid>
       <div class="d-flex justify-space-between align-center w-full">
-        <div class="d-flex align-center ga-4">
+        <div class="d-flex align-center ga-2">
           <v-btn v-if="mobile" @click="toggleNav" icon="mdi-menu"></v-btn>
           <add-chat-block />
         </div>
-        <div class="d-flex align-center ga-4">
+        <div class="d-flex align-center ga-2">
           <v-btn @click="theme.toggle()" :icon="themeIcon"></v-btn>
           <h1 class="d-none d-md-block text-h5">
             Chat with <span class="font-weight-bold font-italic">Stacy</span>
